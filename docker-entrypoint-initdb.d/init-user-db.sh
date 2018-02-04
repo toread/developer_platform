5f907251857b1f6b6ebf1781cede1562b7d00195
toread <lizb1988@vip.qq.com>
2018-02-04 17:17:37 +0800

    增加NG配置

--- docker-entrypoint-initdb.d/init-user-db.sh
+++ docker-entrypoint-initdb.d/init-user-db.sh
@@ -8,4 +8,12 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE USER mattermost PASSWORD 'mattermost';
     CREATE DATABASE mattermost;
     GRANT ALL PRIVILEGES ON DATABASE mattermost TO mattermost;
+    -- sonarqube
+    CREATE USER sonarqube PASSWORD 'sonarqube';
+    CREATE DATABASE sonarqube;
+    GRANT ALL PRIVILEGES ON DATABASE sonarqube TO sonarqube;
+    -- owncloud
+    CREATE USER owncloud PASSWORD 'owncloud';
+    CREATE DATABASE owncloud;
+    GRANT ALL PRIVILEGES ON DATABASE owncloud TO owncloud;
 EOSQL
